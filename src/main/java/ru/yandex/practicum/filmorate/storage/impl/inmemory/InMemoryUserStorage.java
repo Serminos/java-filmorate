f9a6439170bdf4adde106ed8591c63811c1c5af8
naviwe <timurnovotorzhin@yandex.kz>
2025-01-15 17:40:20 +0400

    fix: После ревью

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/inmemory/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/inmemory/InMemoryUserStorage.java
@@ -3,14 +3,12 @@ package ru.yandex.practicum.filmorate.storage.impl.inmemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
