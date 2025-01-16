3804e2c44f24e9b530480baf9d3beef67a5b2c17
KuznetsovOS <KuznetsovOS@kadastr.ru>
2025-01-17 02:28:12 +0300

    feat: Функциональность «Поиск»

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDirectorStorage.java
@@ -1,13 +1,17 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.dto.DirectorDto;
+import ru.yandex.practicum.filmorate.model.FilmDirector;
 
+import java.util.List;
 import java.util.Set;
 
 public interface FilmDirectorStorage {
 
-        void create(Long id, Set<DirectorDto> directors);
+    void create(Long id, Set<DirectorDto> directors);
 
-        void deleteByFilmId(Long id);
+    void deleteByFilmId(Long id);
+
+    List<FilmDirector> findByDirectorIdIn(List<Long> directorIds);
 
 }
